6b11d42dac4d9c896c823511461620c2f8ef040d
Eliot Wong-Toi <eliotwong-toi@Eliots-MacBook-Pro.local>
2021-11-26 14:48:04 -0800

    added code to plot attempts/pcts by player

--- identify_players.R
+++ identify_players.R
@@ -29,10 +29,50 @@ thirteenclub <- joined_shots %>%
 
 View(thirteenclub)
 
+write.csv(thirteenclub, "~/Documents/uci/nba_shot_charts/data/thirteenclub.csv")
 
-small13 <- thirteenclub %>% dplyr::select(ends_with("attempt"))
-View(small13)
+small13 <- thirteenclub %>% dplyr::select(ends_with("attempt") | ends_with("pct"))
+
 
 as.matrix(dist(small13, "euclidean"))
 
 # probably want to take demar, steph, kat (reasonably distant)
+interesting_3 <- c("Stephen Curry", "DeMar DeRozan", "Karl-Anthony Towns")
+
+small13$player_name <- thirteenclub$PLAYER_NAME
+
+df_att <- melt(small13 %>% 
+            dplyr::filter(player_name %in% interesting_3) %>% 
+            dplyr::select(ends_with("attempt") | "player_name"), 
+          shot = colnames(smallthirteen), 
+          variable.name="shot")
+
+df_pct <- melt(small13 %>% 
+                 dplyr::filter(player_name %in% interesting_3) %>% 
+                 dplyr::select(ends_with("pct") | "player_name"), 
+               shot = colnames(smallthirteen), 
+               variable.name="shot")
+
+
+ggplot(df_att, aes(shot, value, fill=player_name)) + 
+  geom_bar(position="dodge",stat="identity") +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + 
+  ggtitle("Shot attempts by region") + 
+  ylab("Attempts") + 
+  xlab("Court Region")
+
+ggplot(df_pct, aes(shot, value, fill=player_name)) + 
+  geom_bar(position="dodge",stat="identity") +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + 
+  ggtitle("Percentages by region") + 
+  ylab("Percentage") + 
+  xlab("Court Region")
+
+
+dft = melt(data.frame(A=c(2, 10), B=c(3, 20), 
+                     experiment=c("X", "X & Y")),
+          variable.name="metric")
+dft
+ggplot(dft, aes(experiment, value, fill=metric)) + 
+  geom_bar(position="dodge",stat="identity")
+
